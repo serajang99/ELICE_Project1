320f4f9240e3e51fd84f239bfae5254fff223322
serajang99 <serajang1679@gmail.com>
2021-11-18 15:44:36 +0900

    대여 기록, 반납하기 기능 구현

--- rent.py
+++ rent.py
@@ -10,13 +10,76 @@ bp = Blueprint("rent", __name__, url_prefix="/rent")
 
 @bp.route('/list', methods=('GET', 'POST'))
 def list():
-    return render_template('rent.html')
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute('SELECT id FROM user WHERE email = %s',
+                   (session['email']))
+    user_id = cursor.fetchone()
+
+    cursor.execute(
+        f'SELECT book_id FROM bookRental WHERE user_id = %s', (user_id['id'])
+    )
+    book_id = cursor.fetchall()
+    print(book_id)
+    books = []
+    for id in book_id:
+        cursor.execute(
+            f'SELECT * FROM book WHERE id = %s', (id['book_id'])
+        )
+        books.append(cursor.fetchone())
+
+    print(books)
+    return render_template('rent_list.html', books=books)
 
 
-@bp.route('/ret', methods=('GET', 'POST'))
-def ret():
+@bp.route('/ret/<id>', methods=('GET', 'POST'))
+def ret(id):
 
-    return render_template('rent.html')
+    if id == '-1':
+        db = get_db()
+        cursor = db.cursor()
+
+        cursor.execute('SELECT id FROM user WHERE email = %s',
+                       (session['email']))
+        user_id = cursor.fetchone()
+
+        cursor.execute(
+            f'SELECT book_id FROM bookRental WHERE user_id = %s', (
+                user_id['id'])
+        )
+        book_ids = cursor.fetchall()
+        # print(book_id)
+        books = []
+        for book_id in book_ids:
+            cursor.execute(
+                f'SELECT * FROM book WHERE id = %s', (book_id['book_id'])
+            )
+            books.append(cursor.fetchone())
+
+        # print(books)
+        return render_template('rent_ret.html', books=books)
+
+    else:
+        db = get_db()
+        cursor = db.cursor()
+
+        cursor.execute('SELECT id FROM user WHERE email = %s',
+                       (session['email']))
+        user_id = cursor.fetchone()['id']
+        print(user_id)
+
+        cursor.execute(
+            f'DELETE FROM bookRental WHERE book_id = %s', (id)
+        )
+        db.commit()
+
+        cursor.execute(
+            f'UPDATE book SET stock=stock+1 WHERE id=%s', (id)
+        )
+        db.commit()
+
+        return redirect('/rent/list')
 
 
 @bp.route('/rent/<id>', methods=('GET', 'POST'))
