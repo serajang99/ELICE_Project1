a4352662675415f1a087f837d6309c08d0d35288
serajang99 <serajang1679@gmail.com>
2021-11-26 23:10:37 +0900

    add main.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, g, session
 from db_connect import db
 from models import Book
-from . import update_data
 from . import update_data_library
 from . import auth
 from . import book
@@ -10,44 +9,44 @@ from . import rent
 from . import manage
 from datetime import datetime
 
-app = Flask(__name__)
 
-app.config.from_mapping(
-    SECRET_KEY='dev',
-)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/elice_library'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app(test_config=None):
+    app = Flask(__name__)
 
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/elice_library'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(auth.bp)
-app.register_blueprint(book.bp)
-app.register_blueprint(rent.bp)
-app.register_blueprint(search.bp)
-app.register_blueprint(manage.bp)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(book.bp)
+    app.register_blueprint(rent.bp)
+    app.register_blueprint(search.bp)
+    app.register_blueprint(manage.bp)
 
-db.init_app(app)
+    db.init_app(app)
 
+    @app.route('/')
+    def index():
 
-@app.route('/')
-def index():
+        libraries = update_data_library.update_data_library()
 
-    # update_data.update_data()
-    libraries = update_data_library.update_data_library()
+        page = request.args.get('page', type=int, default=1)  # 페이지
 
-    page = request.args.get('page', type=int, default=1)  # 페이지
+        bookList = Book.query.order_by(Book.id.asc())
+        bookList = bookList.paginate(page, per_page=8)
 
-    bookList = Book.query.order_by(Book.id.asc())
-    bookList = bookList.paginate(page, per_page=8)
+        return render_template('index_list.html', books=bookList, libraries=libraries)
 
-    return render_template('index_list.html', books=bookList, libraries=libraries)
+    @app.route('/popup')
+    def popup():
+        session['cnt'] = 1
+        day = datetime.today().day - 3
+        today = datetime.today().strftime("%Y-%m-")+str(day)+" 00:00:00"
+        bookList = Book.query.filter(
+            Book.register_time >= today).all()
 
+        return render_template('popup.html', books=bookList)
 
-@app.route('/popup')
-def popup():
-    session['cnt'] = 1
-    day = datetime.today().day - 3
-    today = datetime.today().strftime("%Y-%m-")+str(day)+" 00:00:00"
-    bookList = Book.query.filter(
-        Book.register_time >= today).all()
-
-    return render_template('popup.html', books=bookList)
+    return app
